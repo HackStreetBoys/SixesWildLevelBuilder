8899d215b58ba57c4214a75c2dfcbb485c470267
timpetrigit config --global user.name timpetrigit config --global user.name timpetri <tapetri@wpi.edu>
2015-04-14 22:03:27 -0400

    Splash screen functionality.

--- hackstreet/levelbuilder/main/SWLevelBuilderRunner.java
+++ hackstreet/levelbuilder/main/SWLevelBuilderRunner.java
@@ -2,4 +2,16 @@ package hackstreet.levelbuilder.main;
 
 public class SWLevelBuilderRunner {
 
+	
+	
+	public static void main(String[] args) {
+		
+		// display splash screen for 5 seconds
+		SplashScreen splash = new SplashScreen(5000, "images/LevelBuilderSplashScreen.png");
+		splash.showSplash();
+	}
+	
+	
+	
+	
 }
