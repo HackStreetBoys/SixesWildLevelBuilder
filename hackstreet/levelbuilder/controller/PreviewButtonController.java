19943e93d3b9d75a99f56bdadc22510e8850d832
nsbradford <nsbradford@gmail.com>
2015-04-15 10:38:12 -0400

    Added Level Editor controllers, basic Preview Screen.

--- hackstreet/levelbuilder/controller/PreviewButtonController.java
+++ hackstreet/levelbuilder/controller/PreviewButtonController.java
@@ -1,5 +1,31 @@
 package hackstreet.levelbuilder.controller;
 
-public class PreviewButtonController {
+import hackstreet.levelbuilder.gui.LevelBuilderApplication;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+/**
+ * On button press, go to Passive Game Screen.
+ * @author Nicholas
+ */
+public class PreviewButtonController implements ActionListener{
+
+	LevelBuilderApplication application;
+	
+	/**
+	 * Constructor
+	 * @param application
+	 */
+	public PreviewButtonController(LevelBuilderApplication application){
+		this.application = application;
+	}
+	
+	/**
+	 * On button press, go to Passive Game Screen.
+	 */
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		application.enterPassiveGameScreen();
+	}
 
 }
--- hackstreet/levelbuilder/controller/RemoveTileCheckController.java
+++ hackstreet/levelbuilder/controller/RemoveTileCheckController.java
@@ -1,5 +1,26 @@
 package hackstreet.levelbuilder.controller;
 
-public class RemoveTileCheckController {
+import hackstreet.levelbuilder.gui.LevelBuilderApplication;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JCheckBox;
 
+public class RemoveTileCheckController implements ActionListener {
+
+	LevelBuilderApplication application;
+	
+	public RemoveTileCheckController(LevelBuilderApplication application){
+		this.application = application;
+	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		JCheckBox checkBox = (JCheckBox)(e.getSource());
+		if (checkBox.isSelected()){
+			application.model.getActiveLevel().setNumRemove(1);
+		}
+		else{
+			application.model.getActiveLevel().setNumRemove(0);
+		}			
+	}
 }
