f4975e73b8ad473071e42760abc5ceef9748123b
neoterraforma <himanshu23@icloud.com>
2015-04-14 23:31:14 -0400

    Current working version extending AbstractScreen

--- hackstreet/levelbuilder/gui/LevelManagerScreen.java
+++ hackstreet/levelbuilder/gui/LevelManagerScreen.java
@@ -1,5 +1,16 @@
 package hackstreet.levelbuilder.gui;
+import javax.swing.JPanel;
 
-public class LevelManagerScreen {
-
+@SuppressWarnings("serial")
+public class LevelManagerScreen extends AbstractScreen{
+	public LevelManagerScreen(LevelBuilderApplication application) {
+		
+		
+		
+		setLayout(null);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 200, 600);
+		add(panel);
+	}
 }
