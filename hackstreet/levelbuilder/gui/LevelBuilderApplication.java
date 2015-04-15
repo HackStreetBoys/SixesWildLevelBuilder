c2a03d7c4ff0f1f11abcea7372cd15906b5d1b50
nsbradford <nsbradford@gmail.com>
2015-04-14 23:11:55 -0400

    LevelBuilderApplication update.

--- hackstreet/levelbuilder/gui/LevelBuilderApplication.java
+++ hackstreet/levelbuilder/gui/LevelBuilderApplication.java
@@ -1,5 +1,58 @@
 package hackstreet.levelbuilder.gui;
+import javax.swing.JFrame;
 
-public class LevelBuilderApplication {
+import hackstreet.levelbuilder.main.SWLevelBuilder;
 
+/**
+ * 
+ * @author Nicholas
+ *
+ */
+@SuppressWarnings("serial")
+public class LevelBuilderApplication extends JFrame{
+
+	/** Game model. */
+	SWLevelBuilder model;
+	
+	/** Main Screen GUI. */
+	LBMainScreen mainScreen;
+	
+	/** Level manager screen GUI. */
+	LevelManagerScreen levelManagerScreen;
+	
+	/** Level manager screen GUI. */
+	LevelEditorScreen levelEditorScreen;
+	
+	/** Level manager screen GUI. */
+	AbstractScreen activeScreen;
+	
+	/**
+	 * 
+	 * @param model
+	 */
+	public LevelBuilderApplication(SWLevelBuilder model){
+		this.model = model;
+	}
+	
+	/**
+	 * 
+	 */
+	void enterMainScreen(){
+		this.activeScreen = this.mainScreen;
+	}
+
+	/**
+	 * 
+	 */
+	void enterLevelManagerScreen(){
+		this.activeScreen = this.levelManagerScreen;
+	}
+	
+	/**
+	 * 
+	 */
+	void enterLevelEditorScreen(){
+		this.activeScreen = this.levelEditorScreen;
+	}
+	
 }
--- hackstreet/levelbuilder/main/SWLevelBuilderRunner.java
+++ hackstreet/levelbuilder/main/SWLevelBuilderRunner.java
@@ -2,16 +2,4 @@ package hackstreet.levelbuilder.main;
 
 public class SWLevelBuilderRunner {
 
-	
-	
-	public static void main(String[] args) {
-		
-		// display splash screen for 5 seconds
-		SplashScreen splash = new SplashScreen(5000, "images/LevelBuilderSplashScreen.png");
-		splash.showSplash();
-	}
-	
-	
-	
-	
 }
