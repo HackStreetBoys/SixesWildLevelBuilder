e92fe83f918fd5106005753b6e72440564ad8698
neoterraforma <himanshu23@icloud.com>
2015-04-14 22:53:53 -0400

    Created LBMainScreen GUI

--- hackstreet/levelbuilder/gui/LBMainScreen.java
+++ hackstreet/levelbuilder/gui/LBMainScreen.java
@@ -1,5 +1,33 @@
 package hackstreet.levelbuilder.gui;
 
-public class LBMainScreen {
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
+@SuppressWarnings("serial")
+public class LBMainScreen extends AbstractScreen{
+	public LBMainScreen(LevelBuilderApplication application) {
+		//super(application);
+
+		setLayout(null);
+
+		JButton btnCreateNew = new JButton("Create New");
+		btnCreateNew.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		btnCreateNew.setBounds(195, 150, 400, 100);
+		add(btnCreateNew);
+
+		JButton btnEditExisting = new JButton("Edit Existing");
+		btnEditExisting.setBounds(195, 260, 400, 100);
+		add(btnEditExisting);
+
+		JButton btnLevelManagement = new JButton("Level Management");
+		btnLevelManagement.setBounds(195, 370, 400, 100);
+		add(btnLevelManagement);
+
+
+
+	}
 }
