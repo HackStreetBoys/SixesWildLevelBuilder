d625a0efd1fccafc03fc30f59ceba8c1cb3cd181
nsbradford <nsbradford@gmail.com>
2015-04-14 23:55:20 -0400

    Removed .levelbuilder package with its duplicate class.

--- hackstreet/levelbuilder/main/SWLevelBuilderRunner.java
+++ hackstreet/levelbuilder/main/SWLevelBuilderRunner.java
@@ -4,10 +4,10 @@ import hackstreet.levelbuilder.main.*;
 
 public class SWLevelBuilderRunner {
 	
-	
 	public static void main(String [] args) {
 		SplashScreen splash = new SplashScreen(5000, "images/LevelBuilderSplashScreen.png");
 		splash.showSplash();
 
+		
 	}
 }
