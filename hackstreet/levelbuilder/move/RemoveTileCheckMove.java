18232c97c88f172ecc155a7384b75d64a3102b7e
neoterraforma <himanshu23@icloud.com>
2015-04-27 16:50:30 -0400

    Added doMove() and undoMove() logic for RemoveTilesCheckMove

--- hackstreet/levelbuilder/move/RemoveTileCheckMove.java
+++ hackstreet/levelbuilder/move/RemoveTileCheckMove.java
@@ -1,21 +1,31 @@
 package hackstreet.levelbuilder.move;
 
+import javax.swing.JCheckBox;
+
+import hackstreet.levelbuilder.main.SWLevelBuilder;
+
 public class RemoveTileCheckMove implements IMove{
 
-	public RemoveTileCheckMove() {
-		
+	//Think about how to implement the stack here
+	
+	SWLevelBuilder model;
+	JCheckBox checkBox;
+	boolean before;
+	boolean after;
+	
+	public RemoveTileCheckMove(SWLevelBuilder model, JCheckBox checkBox) {
+		this.model = model;
+		this.checkBox = checkBox;
 	}
 	
 	@Override
 	public void doMove() {
-		// TODO Auto-generated method stub
-		
+		checkBox.setActionCommand("disable");
 	}
 
 	@Override
 	public void undoMove() {
-		// TODO Auto-generated method stub
-		
+		checkBox.setActionCommand("enable");
 	}
 
 }
